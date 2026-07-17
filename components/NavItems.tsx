231a372abff71350683c4ecd6fa26e925feaa26d
savvyaquib <itsmohammadaquib@gmail.com>
2026-07-17 17:56:14 +0530

    feat: refactor NavItems component for improved navigation and update Navbar import

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,11 +1,27 @@
+"use client";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navItems = [
+  { label: "Home", href: "/" },
+  { label: "Companions", href: "/companions" },
+  { label: "My Journey", href: "/my-journey" },
+];
 
 const NavItems = () => {
+  const pathName = usePathname(); // Get the current pathname
   return (
-    <div>
-      <Link href="/">Home</Link>
-      <Link href="/companions">Companions</Link>
-      <Link href="/my-journey">My Journey</Link>
+    <div className="flex items-center gap-8">
+      {navItems.map((item) => (
+        <Link
+          key={item.href}
+          href={item.href}
+          className={cn(pathName === item.href && "text-primary font-semibold")}
+        >
+          {item.label}
+        </Link>
+      ))}
     </div>
   );
 };
